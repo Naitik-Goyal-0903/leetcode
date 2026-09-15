f510870da0af15b862ff5bdb4339eae8b75b1029
Naitik-Goyal-0903 <goyalnaitik71@gmail.com>
2026-09-15 15:23:15 +0530

    Time: 1 ms (88.31%), Space: 43.4 MB (25.17%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.java
+++ 0520-detect-capital/0520-detect-capital.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        String lower = word.toLowerCase();
-        String upper = word.toUpperCase();
-       String a = word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-        boolean flag =false;
-        if(word.equals(lower)||word.equals(upper)||word.equals(a))
-        {
-            flag = true;
+        int capitals = 0;
+        int n = word.length();
+        for (int i = 0; i < n; i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                capitals++;
+            }
         }
-        return flag;
-       
+        return capitals == n || capitals == 0 || (capitals == 1 && Character.isUpperCase(word.charAt(0)));
     }
 }
