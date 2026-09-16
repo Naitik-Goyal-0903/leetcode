6a5291682c7b331474910bdc1f79c86338f0eae6
Naitik-Goyal-0903 <goyalnaitik71@gmail.com>
2026-09-16 23:24:47 +0530

    Time: 11 ms (7.16%), Space: 45.5 MB (78.44%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean judgeCircle(String moves) {
         Boolean flag = false;
         int Ucount =0;
-        int Lcount =0;int Dcount =0;int Rcount =0;
+        int Lcount =0;
         for (int i =0;i<moves.length();i++)
         {
             if (moves.charAt(i)=='U')
@@ -11,18 +11,18 @@ class Solution {
             }
             if (moves.charAt(i)=='R')
             {
-                Rcount++;
+                    Lcount--;
             }
             if (moves.charAt(i)=='D')
             {
-                Dcount++;
+                Ucount--;
             }
             if (moves.charAt(i)=='L')
             {
                 Lcount++;
             }
         }
-        if(Ucount==Dcount && Rcount==Lcount)
+        if(Ucount==0 && Lcount==0)
             {
                 flag = true;
             }
